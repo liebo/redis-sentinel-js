a98427dc39140a07095ce90d17cca48987369ff3
Stanton W. Jones <stantonwjones@gmail.com>
2013-03-13 18:13:43 -0400

    remove extraneous object exposure in index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-/*
-
-define = define || function () {};
-
-define([], function () {
-    return (module.exports = function () {
-        //setup goes in here
-        this.method = function () {
-            console.log("o hai");
-        }
-    })();
-});
-    */
-
-var NegotiatorClient = require("./negotiator_client.js");
 var Monitor = require("./monitor.js");
 
 exports.Monitor = Monitor;
